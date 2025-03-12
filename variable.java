799be3399475fa2e3c4949bee3922b7334f6b47f
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-12 15:33:28 +0530

    fruits

---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+        public static void main(String args[]){
+           String a="apple ";
+           String b="mango";
+            System.out.println(a.concat(b));
+    
+        }
+    }
