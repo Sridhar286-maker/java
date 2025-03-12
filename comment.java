e5c38abf760d449992d2ce115d9a2f1859822ff2
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-12 15:05:22 +0530

    consistency is the key

---
+++ comment.java
@@ -0,0 +1,14 @@
+public class comment {
+    public static void main(String args[]){
+        System.out.println("consistency");
+
+
+    /*System.out.println("consistency");
+   System.out.println("consistency");
+    System.out.println("consistency");
+    System.out.println("consistency");
+    System.out.println("consistency");
+    System.out.println("consistency");*/
+    
+    }
+}
