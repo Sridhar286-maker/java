a20822a8888151fda6e8cd4444caf2aaf3da5aa3
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-12 15:50:53 +0530

    add

---
+++ primitive.java
@@ -0,0 +1,12 @@
+public class primitive {
+    public static void main(String args[]){
+       String a="apple ";
+       String b="mang";
+        System.out.println(a.concat(b));
+        int a1=5;
+        int a2=6;
+        System.out.println(a1+a2);
+
+    }
+}
+
