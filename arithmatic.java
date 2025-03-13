a557c11da27c6f085662310ec33df74f04a41d93
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 11:14:20 +0530

    add and multi

---
+++ arithmatic.java
@@ -0,0 +1,11 @@
+public class arithmatic {
+    public static void main(String args[]){
+        int a=10;
+        int b=35;
+        System.out.println(a+b);
+        float x=2.345f;
+        int y=56;
+        System.out.println(x*y);
+    }
+    
+}
