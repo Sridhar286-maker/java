cbed8b8a8d7b47a09d2da7fd817354091093da88
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 10:05:10 +0530

    about the student

--- student.java
+++ student.java
@@ -1,13 +1,13 @@
 public class student {
     public void main(String args[]){
         String name="sena";
-        System.out.println(name);
+        System.out.println("Enter your name:"+name);
         String section="cse-b";
-        System.out.println(section);
+        System.out.println("Enter your section:"+section);
         int mark=25;
-        System.out.println(mark);
+        System.out.println("Enter your mark:"+mark);
         char grade='b';
         System.out.println(grade);
-        
+
     }
 }
