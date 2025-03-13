d8003058148a5109099a724324f7b6cbcb86a7e8
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 12:05:12 +0530

    compare two strings

---
+++ logical.java
@@ -0,0 +1,10 @@
+public class logical {
+    public static void main(String[] args) {
+        int a=24;
+        int b=34;
+        System.out.println(a==b&&a<b);
+        int a1=34;
+        int a2=29;
+        System.out.println(a1>=a2||a2<a1);
+    }
+}
