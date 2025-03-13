774a1230bcd2f381bcd62a5dddb922839c51cf42
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 14:45:57 +0530

    if and else method

---
+++ conditional.java
@@ -0,0 +1,12 @@
+public class conditional {
+    public static void main(String[] args) {
+        int age=26;
+        if(age>=18){
+          String  result="He is major";
+        System.out.println(result);
+    }
+    else{
+        System.out.println("He is not a major");
+    }
+} 
+}
