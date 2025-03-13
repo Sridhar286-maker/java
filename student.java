a5720ddca096cef8e7facc7b747488de99cfdf4c
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 09:58:05 +0530

    about student

---
+++ student.java
@@ -0,0 +1,13 @@
+public class student {
+    public void main(String args[]){
+        String name="sena";
+        System.out.println(name);
+        String section="cse-b";
+        System.out.println(section);
+        int mark=25;
+        System.out.println(mark);
+        char grade='b';
+        System.out.println(grade);
+        
+    }
+}
