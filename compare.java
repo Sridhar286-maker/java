1ab814f88a586e2e57772845041651e938caec05
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-13 11:47:19 +0530

    comparing equal or not

---
+++ compare.java
@@ -0,0 +1,11 @@
+public class compare {
+    public static void main(String[] args) {
+        String a1=("apple");
+        String a2=("apple");
+        System.out.println(a1==a2);
+        int a=23;
+        int b=32;
+        System.out.println(a<=b);
+    }
+    
+}
