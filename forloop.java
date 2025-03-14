62875ebab212c2d561dbe9cd86c5e41b73bd330b
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-14 12:21:50 +0530

    add for loop

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop {
+    public static void main(String[] args) {
+        String a="sridhar";
+        int i;
+        for(i=0;i<=5;i++)
+        System.out.println(a);
+    }
+
+   } 
