8733b9a1fe48effe6deb738355c11ad89eb8bb92
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-14 13:28:17 +0530

    method function

---
+++ method.java
@@ -0,0 +1,11 @@
+public class method {
+    public static void main(String args[]){
+        int a=4;
+        int b=45;
+        multiply(a,b);
+    }
+    static void multiply(int a,int b){
+        System.out.println(a*b);
+    }
+    
+}
