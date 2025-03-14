c6f7e9900250f2148a1e30b339af2dbbd8a2cb02
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-14 13:56:17 +0530

    find length

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String args[]){
+        String [] a={"apple","mango","grapes"};
+        int [] b={1,2,3,4,5};
+        System.out.println(a.length);
+    }
+    
+}
