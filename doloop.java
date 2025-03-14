f63afbdc246185b7162dc308d6ea3dab59092e8b
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-14 11:04:25 +0530

    do while loop

---
+++ doloop.java
@@ -0,0 +1,10 @@
+public class doloop {
+    public static void main(String[] args) {
+        int i=0,n=5;
+        do { 
+            System.out.println(n);
+            i=i+1;
+        } while (i<5);
+    }
+    
+}
