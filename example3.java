e0f7fc9b9081c8c72e231f644ae2a4f451aa0b73
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-15 11:29:43 +0530

    switch statement

---
+++ example3.java
@@ -0,0 +1,24 @@
+public class example3 {
+    public static void main(String[] args) {
+        int coffee=3;
+        switch (coffee)
+        {
+            case 1 :{
+                System.out.println("black coffee");
+                break;
+                }
+            case 2:
+                 System.out.println("white coffee");
+                 break;
+            case 3:
+                System.out.println("tea");
+                break;
+            case 4:
+                System.out.println("normal coffee");
+                break;
+            default :
+                System.out.println("there is no coffee");
+        }
+
+    }
+}
