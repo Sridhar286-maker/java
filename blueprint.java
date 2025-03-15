e69f406941c13c168d88221c9df0145809424217
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-15 09:40:05 +0530

    acceswsing sub class and main class

---
+++ blueprint.java
@@ -0,0 +1,12 @@
+class student {
+    int id;
+    String name;
+}    
+public class blueprint{
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.id=101;
+        System.out.println(s1.id);
+    }
+}
+
