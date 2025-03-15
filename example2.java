a08ca502f6199d90b5fafbba53d5db0a1383cb01
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-15 11:01:03 +0530

    placing price amount in array

---
+++ example2.java
@@ -0,0 +1,11 @@
+public class example2 {
+    public static void main(String[] args) {
+        int i;
+        int [] a={100,200,150,250,300};
+        String [] b={"apple","mango","grapes","orange","pine"};
+        for(i=0;i<5;i++)
+        System.out.println(b[i]+"\t="+a[i]);
+        
+
+    }
+}
