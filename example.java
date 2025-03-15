e395abe023307df7c7b79f7dfb46a4ba88bae9b4
Sridhar286-maker <smssridharss123@gmail.com>
2025-03-15 10:29:39 +0530

    by using array to make for loop

---
+++ example.java
@@ -0,0 +1,9 @@
+public class example {
+    public static void main(String[] args) {
+        int i;
+        int [] a={101,102,304,267,132};
+        for(i=0;i<5;i++){
+        System.out.println(a[i]);
+        }
+    }
+}
